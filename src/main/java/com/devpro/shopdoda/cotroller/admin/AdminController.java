1d0488a08a1b712f97d5a22224c40da0d1e2350a
Duong Xuan Da <xuandapa@gmail.com>
2021-05-03 22:11:26 +0700

    update chart admin

--- src/main/java/com/devpro/shopdoda/cotroller/admin/AdminController.java
+++ src/main/java/com/devpro/shopdoda/cotroller/admin/AdminController.java
@@ -3,13 +3,20 @@ package com.devpro.shopdoda.cotroller.admin;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.devpro.shopdoda.services.SaleorderService;
+
 @Controller
 public class AdminController {
+	
+	@Autowired
+	private SaleorderService saleorderService;
+	
 	/**
 	 * @param model    - Dùng để đẩy dữ liệu hoặc hứng dữ liệu từ tầng VIEW.
 	 * @param request  - Các thông tin người dùng yêu cầu.
@@ -20,6 +27,20 @@ public class AdminController {
 	@RequestMapping(value = { "/admin", "/dashboard", "/quantri" }, method = RequestMethod.GET)
 	public String index(final ModelMap model, final HttpServletRequest request, final HttpServletResponse response)
 			throws Exception {
+		
+		/*
+		 * StringBuilder myData = new StringBuilder(); myData.append("["); for (int i =
+		 * 1; i <= 11; i++) { myData.append("1000"+","); } myData.append("1000");
+		 * myData.append("]");
+		 * 
+		 * int[] mydata = new int[12]; for (int i = 0; i <= 11; i++) { mydata[i] = 1000;
+		 * }
+		 */
+		
+		model.addAttribute("myData", saleorderService.thongKeTheoThang());
+		model.addAttribute("doanhThuNam", saleorderService.doanhThuNam());
+		model.addAttribute("doanhThuThang", saleorderService.doanhThuThang());
+		model.addAttribute("doanhThuNgay", saleorderService.doanhThuNgay());
 		return "back-end/index";
 	}
 
--- src/main/java/com/devpro/shopdoda/services/SaleorderService.java
+++ src/main/java/com/devpro/shopdoda/services/SaleorderService.java
@@ -1,5 +1,8 @@
 package com.devpro.shopdoda.services;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -9,6 +12,7 @@ import javax.persistence.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import com.devpro.shopdoda.dto.TKThang;
 import com.devpro.shopdoda.dto.search.SaleorderSearch;
 import com.devpro.shopdoda.entities.Saleorder;
 import com.devpro.shopdoda.taglibs.PaginationTaglib;
@@ -40,4 +44,119 @@ public class SaleorderService {
 		}
 		return query.getResultList();
 	}
+
+	public String thongKeTheoThang() {
+		StringBuilder data = new StringBuilder();
+
+		String nativeSql = "SELECT MONTH(s.created_date), SUM(sp.price_unit) "
+				+ "FROM tbl_saleorder s, tbl_saleorder_products sp " 
+				+ "WHERE s.id = sp.saleorder_id "
+				+ "GROUP BY MONTH(s.created_date) " 
+				+ "ORDER BY MONTH(s.created_date) ";
+
+		Query query = entityManager.createNativeQuery(nativeSql);
+
+		List<Object[]> lstObj = query.getResultList();
+
+		List<TKThang> lst = new ArrayList<TKThang>();
+		for (Object[] object : lstObj) {
+			TKThang tkThang = new TKThang();
+			tkThang.setThang((int) object[0]);
+			tkThang.setTotal(((BigDecimal) object[1]).intValue());
+			lst.add(tkThang);
+		}
+
+		int[] dataInt = new int[12];
+		for (int i = 0; i < lst.size(); i++) {
+			for (int j = 0; j < 12; j++) {
+				if (lst.get(i).getThang() == j + 1) {
+					dataInt[j] = lst.get(i).getTotal();
+					break;
+				}
+			}
+		}
+		data.append("[");
+		for (int i = 0; i < dataInt.length - 1; i++) {
+			data.append(dataInt[i] + ",");
+		}
+		data.append(dataInt[dataInt.length - 1]);
+		data.append("]");
+
+		return data.toString();
+	}
+
+	public Integer doanhThuNgay() {
+		Integer data = 0;
+
+		int dayOfMonth = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+		int month = Calendar.getInstance().get(Calendar.MONTH);
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+
+		String nativeSql = "SELECT SUM(s.total) " 
+				+ "FROM tbl_saleorder s "
+				+ "WHERE 1=1 " 
+				+ "AND DAY(s.created_date) = " + dayOfMonth + " "
+				+ "AND MONTH(s.created_date) = " + (month+1) + " " 
+				+ "AND YEAR(s.created_date) = " + year + " ";
+
+		Query query = entityManager.createNativeQuery(nativeSql);
+
+		try {
+//			List<BigDecimal> listData= query.getResultList();
+//			for (BigDecimal dataItem : listData) {
+//				data += dataItem.intValue();
+//			}
+			data = ((BigDecimal) query.getSingleResult()).intValue();
+		} catch (NullPointerException e) {
+			return 0;
+		}
+		return data;
+	}
+
+	public Integer doanhThuThang() {
+		Integer data = 0;
+
+		int month = Calendar.getInstance().get(Calendar.MONTH);
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+
+		String nativeSql = "SELECT SUM(s.total) " 
+				+ "FROM tbl_saleorder s "
+				+ "WHERE 1=1 " 
+				+ "AND MONTH(s.created_date) = " + (month+1) + " "
+				+ "AND YEAR(s.created_date) = " + year + " ";
+
+		Query query = entityManager.createNativeQuery(nativeSql);
+
+		try {
+//			List<BigDecimal> listData= query.getResultList();
+//			for (BigDecimal dataItem : listData) {
+//				data += dataItem.intValue();
+//			}
+			data = ((BigDecimal) query.getSingleResult()).intValue();
+		} catch (NullPointerException e) {
+			return 0;
+		}
+		return data;
+	}
+
+	public Integer doanhThuNam() {
+		Integer data = 0;
+
+		int year = Calendar.getInstance().get(Calendar.YEAR);
+
+		String nativeSql = "SELECT SUM(sp.price_unit) " 
+				+ "FROM tbl_saleorder s, tbl_saleorder_products sp "
+				+ "WHERE s.id = sp.saleorder_id " 
+				+ "AND YEAR(s.created_date) = " + year + " ";
+
+		Query query = entityManager.createNativeQuery(nativeSql);
+
+		try {
+			data = ((BigDecimal) query.getSingleResult()).intValue();
+		} catch (NullPointerException e) {
+			return 0;
+		}
+		return data;
+	}
+
 }
